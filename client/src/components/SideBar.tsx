7d502c0fca6f23c2111305e7458bb52d7d719305
Khushi <khushichoksi1@gmail.com>
2025-04-07 22:11:47 -0600

    Minor changes

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -3,14 +3,12 @@ import { NavLink } from "react-router-dom";
 
 export default function SideBar() {
 
-
     const createNavLink = (to, label, icon) => (
         <li className="my-2">
             <NavLink
                 to={to}
                 className={({ isActive }) =>
-                    `flex items-center p-2.5 ${isActive ? 'bg-dark_green text-white' : 'text-white hover:text-gray-300'}`
-            }
+                    `flex items-center p-2.5 ${isActive ? 'bg-dark_green text-white' : 'text-white hover:text-light_green'}`}
             >
                 <div className="flex items-center">
                     <span className="mr-4">
@@ -23,16 +21,16 @@ export default function SideBar() {
     );
 
     // icon reference: https://flowbite.com/docs/customize/icons/ 
-    //icon reference: https://windytoolbox.com/icons
+    // icon reference: https://windytoolbox.com/icons
     const dashboardIcon = (
-        <svg className="ml-4 text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <svg className="ml-4" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />  
             <line x1="3" y1="9" x2="21" y2="9" />  
             <line x1="9" y1="21" x2="9" y2="9" />
         </svg>
     );
     const inventoryIcon = (
-        <svg className="ml-4 text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <svg className="ml-4" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <line x1="16.5" y1="9.4" x2="7.5" y2="4.21" />  
             <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />  
             <polyline points="3.27 6.96 12 12.01 20.73 6.96" />  
@@ -40,13 +38,13 @@ export default function SideBar() {
         </svg>
     );
     const requestsIcon = (
-        <svg className="ml-4 text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <svg className="ml-4" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
 <           path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
         </svg>
     );
 
     const vaccineIcon = (
-        <svg className="ml-4 text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <svg className="ml-4" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <line x1="16.5" y1="9.4" x2="7.5" y2="4.21" />  
             <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />  
             <polyline points="3.27 6.96 12 12.01 20.73 6.96" />  
@@ -55,7 +53,7 @@ export default function SideBar() {
     );
 
     const configureIcon = (
-        <svg className="ml-4 text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <svg className="ml-4" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <circle cx="14" cy="6" r="2" />  <line x1="4" y1="6" x2="12" y2="6" />  
             <line x1="16" y1="6" x2="20" y2="6" />  <circle cx="8" cy="12" r="2" />  
             <line x1="4" y1="12" x2="6" y2="12" />  <line x1="10" y1="12" x2="20" y2="12" />  
@@ -65,7 +63,7 @@ export default function SideBar() {
     );
 
     const distributorIcon = (
-        <svg className="ml-4 text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <svg className="ml-4" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />  
             <circle cx="9" cy="7" r="4" />  
             <path d="M23 21v-2a4 4 0 0 0-3-3.87" />  
@@ -73,7 +71,7 @@ export default function SideBar() {
         </svg>
     );
     const settingsIcon = (
-        <svg className="ml-4 text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <svg className="ml-4" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z"/>  
             <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />  
             <circle cx="12" cy="12" r="3" />    
@@ -87,6 +85,7 @@ export default function SideBar() {
                     INOCULYST
                 </h1>
             </div>
+
             <div className="bg-dark1/30 p-6">
                 <p className="font-semibold">Admin name</p>
                 <p className="text-dark_green">Owner</p>
@@ -104,9 +103,6 @@ export default function SideBar() {
             <ul className="mt-auto mb-6">
                 {createNavLink('/account', 'Account Settings', settingsIcon)}
             </ul>
-
-
-
         </nav>
     );
 
