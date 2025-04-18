45536bd2daafe9211b7250b488545ffac5e1af28
Khushi <khushichoksi1@gmail.com>
2025-04-18 17:39:22 -0600

    Fixed header block issue.

--- client/src/components/HeaderBlock.tsx
+++ client/src/components/HeaderBlock.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function HeaderBlock() {
   return (
-    <div className="bg-light_green/50 p-6 h-20">
+    <div className="bg-light_green/50 p-6 h-[76px]">
             </div>
   );
 }
