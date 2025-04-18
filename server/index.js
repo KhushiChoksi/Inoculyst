0acafd00467745e126f55bb53a5a4ccea4fe3986
Khushi <khushichoksi1@gmail.com>
2025-04-18 16:26:22 -0600

    Connection setup.

--- server/index.js
+++ server/index.js
@@ -4,9 +4,22 @@ const cors = require('cors');
 
 app.use(cors())
 
+const db = require('./db.js');
+
 app.get('/', (req, res) => {
-    res.send('Hello from our server!')
-})
+    res.send('Hello from the server!');
+});  
+
+app.get('/test-db', (req, res) => {
+  db.query('SELECT * FROM ACCOUNT', (err, results) => {
+    if (err) {
+      console.error('DB error:', err);
+      return res.status(500).send('Something broke querying the DB');
+    }
+    res.json(results);
+  });
+});
+
 
 app.listen(8080, () => {
     console.log('server listening on port 8080')
